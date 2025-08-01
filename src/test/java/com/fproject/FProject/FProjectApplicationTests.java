670b23c8d851df7a5b9dfb1df7dac3223f092070
CourtesiLOL <ruiznietojavier@gmail.com>
2025-08-01 11:30:03 +0200

    Init files

---
+++ src/test/java/com/fproject/FProject/FProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fproject.FProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
