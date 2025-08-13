c002ef7463bbdf827eb52c7a7077b1bbe3c89d3c
CourtesiLOL <ruiznietojavier@gmail.com>
2025-08-13 14:00:47 +0200

    FIX: problema del cors

---
+++ src/main/java/com/fproject/FProject/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.fproject.FProject.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ *
+ * @author javier
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${front-url}")
+    private String url;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Permitir todas las rutas
+                .allowedOrigins(url) // Cambia esto por el dominio de tu aplicación React
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                .allowCredentials(true); // Permitir cookies
+    }
+}
